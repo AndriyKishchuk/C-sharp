90d2eb79e49648db4469848442eb3a4a61a37ab4
w72228 <andriy302007@gmail.com>
2026-02-14 04:38:33 +0100

    Update Program.cs

--- Projekt_3/Projekt_3/Program.cs
+++ Projekt_3/Projekt_3/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Projekt_3.Energy;
+using Projekt_3.Logs;
+using Projekt_3.Modules;
+using Projekt_3.Storage;
+using System;
 using System.Xml.Linq;
 
 
@@ -11,7 +15,7 @@ namespace Projekt_3
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             using (StationLog stationLog = new StationLog(@"C:\\Users\\andri\\source\\repos\\C#\\Projekt_3\\Projekt_3\\station_log.txt"))
             {
-                stationLog.WriteLog("Станція запущена.");
+                stationLog.Write("Станція запущена.");
                 LifeSupportModule lifeSupport = new LifeSupportModule("Життєзабезпечення", 50);
                 ScienceModule science = new ScienceModule("Наука", 70);
 
@@ -20,24 +24,24 @@ namespace Projekt_3
                 science.ResearchProjects.Add("Розробка нових матеріалів для космосу");
 
                 lifeSupport.Start();
-                stationLog.WriteLog($"{lifeSupport.Name} запущено.");
+                stationLog.Write($"{lifeSupport.Name} запущено.");
                 science.Start();
-                stationLog.WriteLog($"{science.Name} запущено.");
+                stationLog.Write($"{science.Name} запущено.");
 
                 lifeSupport.PerformDiagnostics();
-                stationLog.WriteLog($"{lifeSupport.Name} провів діагностику.");
+                stationLog.Write($"{lifeSupport.Name} провів діагностику.");
                 science.PerformDiagnostics();
-                stationLog.WriteLog($"{science.Name} провів діагностику.");
+                stationLog.Write($"{science.Name} провів діагностику.");
 
-                stationLog.WriteLog("Перевірка енергосистеми");
+                stationLog.Write("Перевірка енергосистеми");
 
                 SolarPanel solarPanel = new SolarPanel { SunIntensity = 0.85 };
                 NuclearReactor reactor = new NuclearReactor { StabilityLevel = 0.95 };
 
-                stationLog.WriteLog($"{solarPanel.EnergyType}: {solarPanel.GetEnergyOutput()} кВт");
-                stationLog.WriteLog($"{reactor.EnergyType}: {reactor.GetEnergyOutput()} кВт");
+                stationLog.Write($"{solarPanel.EnergyType}: {solarPanel.GetEnergyOutput()} кВт");
+                stationLog.Write($"{reactor.EnergyType}: {reactor.GetEnergyOutput()} кВт");
 
-                stationLog.WriteLog("Керування сховищем");
+                stationLog.Write("Керування сховищем");
 
                 StorageUnit<Cargo> cargoStorage = new StorageUnit<Cargo>();
 
@@ -45,13 +49,13 @@ namespace Projekt_3
                 cargoStorage.AddItem(new Cargo { Name = "Вода",  Weight = 15.0 });
                 cargoStorage.AddItem(new Cargo { Name = "Продукти", Weight = 8.5 });
 
-                stationLog.WriteLog($"Додано {cargoStorage._items.Count} предметів у сховище.");
+                stationLog.Write($"Додано {cargoStorage._items.Count} предметів у сховище.");
 
                 cargoStorage.GetItem(0);
                 cargoStorage.GetItem(1);
                 cargoStorage.GetItem(2);
 
-                stationLog.WriteLog("Симуляція завершена.");
+                stationLog.Write("Симуляція завершена.");
             }
         }
     }
