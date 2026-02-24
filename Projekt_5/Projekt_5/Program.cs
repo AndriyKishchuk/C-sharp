6644eb2375deaad0dfdbad81b3299a95ee934b10
w72228 <andriy302007@gmail.com>
2026-02-24 03:01:11 +0100

    Initial Projekt_5

---
+++ Projekt_5/Projekt_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using Projekt_5;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Warehouse warehouse = new Warehouse();
+        warehouse.OnItemChanged += (index, value) => Console.WriteLine($"Лог: У комірку #{index} додано товар: {value}");
+
+        List<string> strings = new List<string> {"Товар 1", "Товар 2", "", "Товар 3", "Товар 4", "Товар 5"};
+        Predicate<string> isNotEmpty = item => !string.IsNullOrEmpty(item);
+        Func<string, string> toUpper = item => item.ToUpper();
+
+        for (int i = 0; i < strings.Count; i++)
+        {
+            if (isNotEmpty(strings[i]))
+            {
+                string upperString = toUpper(strings[i]);
+                warehouse[i] = upperString;
+            }
+        }
+        Console.WriteLine("Вміст складу: ");
+        for(int i = 0; i < strings.Count; i++)
+        {
+            Console.WriteLine(warehouse[i]);
+        }
+
+    }
+}
---
+++ Projekt_5/Projekt_5/Warehouse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt_5
+{
+    internal class Warehouse
+    {
+        string[] _items = new string[10];
+        public event Action<int, string> OnItemChanged;
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _items.Length)
+                {
+                    throw new IndexOutOfRangeException("Index must be between 0 and 9.");
+                }
+                return $"Item at index {index}: {_items[index]}";
+            }
+            set
+            {
+                if (index < 0 || index >= _items.Length)
+                {
+                    throw new IndexOutOfRangeException("Index must be between 0 and 9.");
+                }
+                _items[index] = value;
+                OnItemChanged?.Invoke(index, value);
+            }
+
+        }
+    }
+}
